64b600f89448e8917c0040501d179db3dd48a402
SRIKANTH R <98140086+srikanth-rl@users.noreply.github.com>
2024-01-19 23:14:31 +0530

    Delete JAVA directory

--- JAVA/Var_Args.java
+++
@@ -1,7 +0,0 @@
-import java.util.Arrays;
-public class Var_Args {
-    public static void GetValues(int...Array){//int Array-->Array[]
-    System.out.println(Arrays.toString(Array));}
-    public static void  main(String...args){
-    GetValues(1,2,3,4,5);}
-}
